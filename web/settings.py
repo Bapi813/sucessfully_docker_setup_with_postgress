71c884a13a542142e38e1bf949c1b0d565e7676a
Bapi813 <bapimahalik51@hotmail.com>
2021-06-17 00:53:18 +0530

    'secure_proxy_ssl'

--- web/settings.py
+++ web/settings.py
@@ -31,7 +31,7 @@ with open(os.path.join(BASE_DIR,'pass.txt')) as f:
 #DEBUG = True
 DEBUG= int(os.environ.get('DEBUG',default=0))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mighty-spire-85831.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -160,6 +160,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Heroku
